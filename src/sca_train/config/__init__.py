932e3d9c5cbc9c8a2bfabfd42b14372097df9f7f
riverfog7 <cjw9770@gmail.com>
2025-11-30 01:02:16 +0900

    Logger

--- src/sca_train/config/__init__.py
+++ src/sca_train/config/__init__.py
@@ -1,20 +1,20 @@
 from enum import Enum
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import BaseModel
 
 
 class SCATrainingVerbosityLevel(int, Enum):
     QUIET = 0
-    NORMAL = 1
-    DETAILED = 2
-    DEBUG = 3
+    ERROR = 1
+    WARNING = 2
+    INFO = 3
+    DEBUG = 4
 
 
 class SCALoraConfig(BaseModel):
     use_qlora: bool = True
-    attn_impl: str = "flash_attention_2"
     r: int = 64
     lora_alpha: int = 128
     target_modules_regex = r"^thinker\.model\.layers\.\d+\..*(q|k|v|o)_proj$"
@@ -23,8 +23,20 @@ class SCALoraConfig(BaseModel):
     task_type: str = "CAUSAL_LM"
 
 
+class SCATrainingArguments(BaseModel):
+
+
+
 class SCATrainingConfig(BaseModel):
+    hf_home: Optional[Path] = Path("./hf_models").absolute()
+    dataset_cache_dir: Optional[Path] = Path("./hf_datasets").absolute()
     model_id: str = "Qwen/Qwen3-Omni-30B-A3B-Instruct"
     train_output_dir: Path = Path("./SCA_finetune")
     verbose: SCATrainingVerbosityLevel = SCATrainingVerbosityLevel.NORMAL
     gradient_checkpointing: bool = True
+    system_prompt: Optional[str] = None
+    instruction_prompt: Optional[str] = None
+    max_length: int = 16384
+    mask_instruction: bool = True
+    attn_impl: str = "flash_attention_2"
+    lora_config: SCALoraConfig = SCALoraConfig()

7595d11924ae9a50e074cca36bedb17aeafbb305
riverfog7 <cjw9770@gmail.com>
2025-11-30 03:17:29 +0900

    Fix pytorch alloc conf warning and pydantic schema error

--- src/sca_train/config/lora.py
+++ src/sca_train/config/lora.py
@@ -7,7 +7,7 @@ class SCALoraConfig(BaseModel):
     use_qlora: bool = True
     r: int = 64
     lora_alpha: int = 128
-    target_modules_regex = r"^thinker\.model\.layers\.\d+\..*(q|k|v|o)_proj$"
+    target_modules_regex: str = r"^thinker\.model\.layers\.\d+\..*(q|k|v|o)_proj$"
     lora_dropout: float = 0.05
     lora_bias: Literal["none", "all", "lora_only"] = "none"
     task_type: str = "CAUSAL_LM"
--- src/sca_train/train.py
+++ src/sca_train/train.py
@@ -1,5 +1,5 @@
 import os
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
 
 import torch
 from peft import LoraConfig, get_peft_model

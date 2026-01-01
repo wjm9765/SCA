491daa4ae36ce1e7bd7716f12f4ace4ac4a5a8d2
riverfog7 <cjw9770@gmail.com>
2026-01-01 21:15:48 +0900

    chore: Change default GPU count for train script and modify some environment variables

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-ACCELERATE_CONFIG="${SCRIPT_DIR}/../configs/accelerate/memory_efficient_2gpus.yaml"
+ACCELERATE_CONFIG="${SCRIPT_DIR}/../configs/accelerate/memory_efficient_4gpus.yaml"
 SCA_CONFIG="${SCRIPT_DIR}/../configs/sca/default.yaml"
-export NCCL_P2P_DISABLE=1
-export NCCL_IB_DISABLE=1
+#export NCCL_P2P_DISABLE=1
+#export NCCL_IB_DISABLE=1
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
+
+export WANDB_PROJECT=${WANDB_PROJECT:-SCA}
 
 cd "${SCRIPT_DIR}/.." || exit 1
 uv run accelerate launch \

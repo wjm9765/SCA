734f1615db48085d5e4cb5e385336974f32acf1b
riverfog7 <cjw9770@gmail.com>
2026-01-14 17:36:32 +0900

    fix: accept both types of config in logger

--- src/sca_train/logger.py
+++ src/sca_train/logger.py
@@ -1,29 +1,49 @@
-from .config import SCATrainingConfig, SCATrainingVerbosityLevel
+from .config import (
+    SCATrainingConfig,
+    SCATrainingVerbosityLevel,
+    SCADuplexTrainingConfig,
+)
 from .utils import get_local_rank
 
 
-def debug(config: SCATrainingConfig, message: str, rank0_only: bool = True) -> None:
+def debug(
+    config: SCATrainingConfig | SCADuplexTrainingConfig,
+    message: str,
+    rank0_only: bool = True,
+) -> None:
     if (
         not rank0_only or get_local_rank() == 0
     ) and config.verbose >= SCATrainingVerbosityLevel.DEBUG:
         print(f"[DEBUG] {message}")
 
 
-def info(config: SCATrainingConfig, message: str, rank0_only: bool = True) -> None:
+def info(
+    config: SCATrainingConfig | SCADuplexTrainingConfig,
+    message: str,
+    rank0_only: bool = True,
+) -> None:
     if (
         not rank0_only or get_local_rank() == 0
     ) and config.verbose >= SCATrainingVerbosityLevel.INFO:
         print(f"[INFO] {message}")
 
 
-def warning(config: SCATrainingConfig, message: str, rank0_only: bool = True) -> None:
+def warning(
+    config: SCATrainingConfig | SCADuplexTrainingConfig,
+    message: str,
+    rank0_only: bool = True,
+) -> None:
     if (
         not rank0_only or get_local_rank() == 0
     ) and config.verbose >= SCATrainingVerbosityLevel.WARNING:
         print(f"[WARNING] {message}")
 
 
-def error(config: SCATrainingConfig, message: str, rank0_only: bool = True) -> None:
+def error(
+    config: SCATrainingConfig | SCADuplexTrainingConfig,
+    message: str,
+    rank0_only: bool = True,
+) -> None:
     if (
         not rank0_only or get_local_rank() == 0
     ) and config.verbose >= SCATrainingVerbosityLevel.ERROR:
